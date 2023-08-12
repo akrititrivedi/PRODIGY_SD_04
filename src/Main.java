ef40b71c4f9ef4021858fb120705a174714d98da
akrititrivedi <akrititrivedi562@gmail.com>
2023-08-12 18:55:54 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check if num is not in current row, current column, and current 3x3 subgrid
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num || board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {  // Empty cell
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {  // Recursively solve
+                                return true;
+                            }
+                            board[row][col] = 0;  // Backtrack if solution not found
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int[] row : board) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] puzzle = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(puzzle)) {
+            System.out.println("Sudoku puzzle solved:");
+            printBoard(puzzle);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+}
